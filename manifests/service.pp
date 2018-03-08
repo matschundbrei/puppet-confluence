68fbc8b71276ae600243f9196109ad30df7e08ea
Jan Kapellen <jan.kapellen@gigacodes.de>
2018-03-08 13:27:35 +0100

    replaced systemd dependency with own exec to relaod systemd

--- manifests/service.pp
+++ manifests/service.pp
@@ -9,16 +9,14 @@ class confluence::service (
   $refresh_systemd       = $confluence::params::refresh_systemd,
 ) {
 
-  if($refresh_systemd) {
-    include ::systemd::systemctl::daemon_reload
-  }
+
 
   file { $service_file_location:
     content => template($service_file_template),
     mode    => '0755',
     notify  => [
       $refresh_systemd ? {
-        true    => Class['systemd::systemctl::daemon_reload'],
+        true    => Exec['systemd_daemon_reload'],
         default => undef
       }
     ],
